85c88bf39db81393118e0a12dc5a0fe709c2318c
Zekcya <118208720+Zekcya@users.noreply.github.com>
2026-01-07 23:51:48 -0600

    Add guild member join/remove handling to manage Minecraft whitelist

--- common/src/main/java/de/erdbeerbaerlp/dcintegration/architectury/overseer/WhitelistListener.java
+++ common/src/main/java/de/erdbeerbaerlp/dcintegration/architectury/overseer/WhitelistListener.java
@@ -7,7 +7,13 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.players.UserWhiteList;
+import net.minecraft.server.players.UserWhiteListEntry;
+import net.minecraft.server.players.NameAndId;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class WhitelistListener extends ListenerAdapter {
 
@@ -51,23 +57,65 @@ public class WhitelistListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberRemove(@NotNull GuildMemberRemoveEvent event) {
-
+        WhitelistData.MinecraftInfo info = WhitelistData.get(event.getUser().getId());
+        if (info != null && DiscordIntegrationMod.server != null) {
+            DiscordIntegrationMod.server.execute(() -> {
+                removeFromWhitelist(info.uuid());
+                DiscordIntegration.LOGGER.info("User {} left Discord. Removed from whitelist", info.name());
+            });
+        }
     }
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
-
+        WhitelistData.MinecraftInfo info = WhitelistData.get(event.getUser().getId());
+        if (info != null && DiscordIntegrationMod.server != null) {
+            DiscordIntegrationMod.server.execute(() -> {
+                addToWhitelist(info.uuid(), info.name());
+                DiscordIntegration.LOGGER.info("User {} joined Discord. Added to whitelist", info.name());
+            });
+        }
     }
 
     private void addToWhitelist(String rawUuid, String name) {
+        try {
+            MinecraftServer server = DiscordIntegrationMod.server;
+            UserWhiteList whitelist = server.getPlayerList().getWhiteList();
 
+            UUID uuid = UUID.fromString(formatUuid(rawUuid));
+            NameAndId key = new NameAndId(uuid, name);
+
+            if (!whitelist.isWhiteListed(key)) {
+                whitelist.add(new UserWhiteListEntry(key));
+                whitelist.save();
+            }
+        } catch (Exception e) {
+            DiscordIntegration.LOGGER.error("Failed to add user to whitelist", e);
+        }
     }
 
     private void removeFromWhitelist(String rawUuid) {
+        try {
+            MinecraftServer server = DiscordIntegrationMod.server;
+            UserWhiteList whitelist = server.getPlayerList().getWhiteList();
 
+            UUID uuid = UUID.fromString(formatUuid(rawUuid));
+            NameAndId key = new NameAndId(uuid, null);
+
+            if (whitelist.isWhiteListed(key)) {
+                whitelist.remove(key);
+                whitelist.save();
+            }
+        } catch (Exception e) {
+            DiscordIntegration.LOGGER.error("Failed to remove user from whitelist", e);
+        }
     }
 
     private String formatUuid(String id) {
-        return id;
+        if (id.contains("-")) return id;
+        return id.replaceFirst(
+                "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
+                "$1-$2-$3-$4-$5"
+        );
     }
 }
